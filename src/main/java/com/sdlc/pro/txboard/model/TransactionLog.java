e06c7214ef07943f21c48d078d51d9e0aa59e88e
mamun <aalmamun295@gmail.com>
2025-08-12 11:25:25 +0600

    enhance TransactionLog to support more information about each transaction

--- src/main/java/com/sdlc/pro/txboard/model/TransactionLog.java
+++ src/main/java/com/sdlc/pro/txboard/model/TransactionLog.java
@@ -1,45 +1,73 @@
 package com.sdlc.pro.txboard.model;
 
-import com.sdlc.pro.txboard.enums.TransactionStatus;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.sdlc.pro.txboard.enums.IsolationLevel;
+import com.sdlc.pro.txboard.enums.PropagationBehavior;
+import com.sdlc.pro.txboard.enums.TransactionPhaseStatus;
 
 import java.io.Serializable;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class TransactionLog implements Serializable {
-    private static final AtomicInteger ATOMIC_TX_ID_GEN = new AtomicInteger(0);
-
-    private final int txId;
+    private final Integer txId;
     private final String method;
+    private final PropagationBehavior propagation;
+    private final IsolationLevel isolation;
     private final Instant startTime;
     private final Instant endTime;
     private final long duration;
+    private final int connectionAcquisitionCount;
+    private final long connectionOccupiedTime;
     private final long alarmingThreshold;
-    private final TransactionStatus status;
+    private final TransactionPhaseStatus status;
     private final String thread;
+    private final List<TransactionLog> child;
+    private final List<String> executedQuires;
+    private final List<TransactionEvent> events;
 
-    public TransactionLog(int txId, String method, Instant startTime, Instant endTime, TransactionStatus status, String thread, long alarmingThreshold) {
+    public TransactionLog(Integer txId, String method, PropagationBehavior propagation, IsolationLevel isolation,
+                          Instant startTime, Instant endTime, int connectionAcquisitionCount,
+                          long connectionOccupiedTime, long alarmingThreshold, TransactionPhaseStatus status,
+                          String thread, List<TransactionLog> child, List<String> executedQuires,
+                          List<TransactionEvent> events) {
         this.txId = txId;
         this.method = method;
+        this.propagation = propagation;
+        this.isolation = isolation;
         this.startTime = startTime;
         this.endTime = endTime;
         this.status = status;
         this.duration = Duration.between(startTime, endTime).toMillis();
-        this.thread = thread;
+        this.connectionAcquisitionCount = connectionAcquisitionCount;
+        this.connectionOccupiedTime = connectionOccupiedTime;
         this.alarmingThreshold = alarmingThreshold;
+        this.thread = thread;
+        this.child = child;
+        this.executedQuires = executedQuires;
+        this.events = events;
     }
 
-    public static TransactionLog from(String method, Instant startTime, Instant endTime, TransactionStatus status, String thread, long alarmingThreshold) {
-        return new TransactionLog(ATOMIC_TX_ID_GEN.incrementAndGet(), method, startTime, endTime, status, thread, alarmingThreshold);
-    }
-
-    public int getTxId() {
+    public Integer getTxId() {
         return txId;
     }
 
     public String getMethod() {
         return method;
+    }
+
+    public PropagationBehavior getPropagation() {
+        return propagation;
+    }
+
+    public IsolationLevel getIsolation() {
+        return isolation;
+    }
+
+    public long getAlarmingThreshold() {
+        return alarmingThreshold;
     }
 
     public Instant getStartTime() {
@@ -50,7 +78,7 @@ public class TransactionLog implements Serializable {
         return endTime;
     }
 
-    public TransactionStatus getStatus() {
+    public TransactionPhaseStatus getStatus() {
         return status;
     }
 
@@ -62,7 +90,39 @@ public class TransactionLog implements Serializable {
         return duration;
     }
 
+    public int getConnectionAcquisitionCount() {
+        return connectionAcquisitionCount;
+    }
+
+    public long getConnectionOccupiedTime() {
+        return connectionOccupiedTime;
+    }
+
     public boolean isAlarming() {
-        return this.duration > this.alarmingThreshold;
+        return this.connectionOccupiedTime > this.alarmingThreshold;
+    }
+
+    public List<TransactionLog> getChild() {
+        return this.child == null ? List.of() : this.child;
+    }
+
+    public List<String> getExecutedQuires() {
+        return this.executedQuires == null ? List.of() : this.executedQuires;
+    }
+
+    public List<TransactionEvent> getEvents() {
+        return events;
+    }
+
+    public int getTotalTransactionCount() {
+        return 1 + getChild().stream()
+                .mapToInt(TransactionLog::getTotalTransactionCount)
+                .sum();
+    }
+
+    public int getTotalQueryCount() {
+        return this.getExecutedQuires().size() + getChild().stream()
+                .mapToInt(TransactionLog::getTotalQueryCount)
+                .sum();
     }
 }
