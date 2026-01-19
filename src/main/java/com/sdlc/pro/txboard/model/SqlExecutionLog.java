245a07b85e29416dd19621065be9626b20c8d44b
mamun <aalmamun295@gmail.com>
2026-01-19 16:56:20 +0600

    update sql execution log model

--- src/main/java/com/sdlc/pro/txboard/model/SqlExecutionLog.java
+++ src/main/java/com/sdlc/pro/txboard/model/SqlExecutionLog.java
@@ -3,20 +3,30 @@ package com.sdlc.pro.txboard.model;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
+import java.util.UUID;
 
 public class SqlExecutionLog {
+    private final UUID id;
     private final Instant conAcquiredTime;
     private final Instant conReleaseTime;
     private final long conOccupiedTime;
+    private final boolean alarmingConnection;
     private final String thread;
     private final List<String> executedQuires;
 
-    public SqlExecutionLog(Instant conAcquiredTime, Instant conReleaseTime, String thread, List<String> executedQuires) {
+    public SqlExecutionLog(UUID id, Instant conAcquiredTime, Instant conReleaseTime, boolean alarmingConnection,
+                           String thread, List<String> executedQuires) {
+        this.id = id;
         this.conAcquiredTime = conAcquiredTime;
         this.conReleaseTime = conReleaseTime;
         this.conOccupiedTime = Duration.between(conAcquiredTime, conReleaseTime).toMillis();
+        this.alarmingConnection = alarmingConnection;
         this.thread = thread;
         this.executedQuires = executedQuires;
+    }
+
+    public UUID getId() {
+        return id;
     }
 
     public Instant getConAcquiredTime() {
@@ -29,6 +39,10 @@ public class SqlExecutionLog {
 
     public long getConOccupiedTime() {
         return conOccupiedTime;
+    }
+
+    public boolean isAlarmingConnection() {
+        return alarmingConnection;
     }
 
     public String getThread() {
